9360e1327a8e250b26d35d41f8b61332f7844744
takaaki.okajima <t.okajima2@gmail.com>
2020-07-04 08:51:19 +0000

    favorites3

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -41,10 +41,10 @@ class MicropostsController extends Controller
     
     public function destroy($id) {
         // idの値で投稿を検索して取得
-        $microposts = \App\Microposts::findOrFail($id);
+        $microposts = \App\Micropost::findOrFail($id);
         
         // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
-        if (\Auth::id === $microposts->user_id) {
+        if (\Auth::id() === $microposts->user_id) {
             $microposts->delete();
         }
         
