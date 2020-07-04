73f0ef12ab3b1178b0ba031a9f5cbe452d2a112c
takaaki.okajima <t.okajima2@gmail.com>
2020-07-04 08:34:25 +0000

    favorites2

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::user()->is_favoriting($micropost->id))
     {{-- アンファボボタンのフォーム --}}
     {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-block"]) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
     {!! Form::close() !!}
 @else
     {{-- ファボボタンのフォーム --}}
     {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-        {!! Form::submit('Favorite', ['class' => "btn btn-light"]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-light btn-sm"]) !!}
     {!! Form::close() !!}
-@endif
+@endif 
